bf647f3e82838838ce880db7453d3eb1ff95095f
Navneet <navneetkumar.html@gmail.com>
2024-12-05 18:32:41 +0530

    mention node version to use

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import {app} from './app.js'
-dotenv.config({
-    path: './.env'
-})
+dotenv.config()
 
 app.listen(process.env.PORT || 8000, () => {
     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
